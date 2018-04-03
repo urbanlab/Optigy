6989bb0ecaae248bf34b07378f6d29d8a696f3e2
Lucas Alloin <lucas.alloin@epitech.eu>
2018-04-03 11:43:40 +0200

    App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ConsoPage } from "../pages/conso/conso";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ConsoPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ConsoPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ConsoPage } from "../conso/conso";
+import * as io from "socket.io-client";
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  socket = io.connect("http://192.168.81.157:3000", { transports: [ "websocket"] });
+  datas;
 
+  constructor(public navCtrl: NavController) {
+    //fill_val();
   }
 
+  ionViewDidLoad(){
+    console.log(this.socket)
+    let tv;
+    let price;
+
+    this.socket.on('connect', function(){
+
+    });
+    this.socket.on('value', function(data){
+      this.datas = data;
+      document.getElementById("whatage").innerText = Math.floor(this.datas.totale + 1) + " kW/h";
+      price = this.datas.totale / 0.1467;
+      document.getElementById("day_price").innerText = Math.floor(price) + " €";
+    });
+  }
+  goToOtherPage() {
+    this.navCtrl.push(ConsoPage)
+  }
 }
